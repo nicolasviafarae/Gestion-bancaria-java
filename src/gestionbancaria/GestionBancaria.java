d81edc83562c68509559537b690e886f8f737b58
nicolasviafarae <nicolasviafarae@gmail.com>
2026-08-27 07:38:06 -0500

    Implementación de menu

--- src/gestionbancaria/GestionBancaria.java
+++ src/gestionbancaria/GestionBancaria.java
@@ -1,20 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package gestionbancaria;
 
-/**
- *
- * @author user
- */
+package gestionbancaria;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 public class GestionBancaria {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in) ;
+        int opcion;
+        boolean bandera = false;
+        System.out.println("-------------BIENVENIDO A SU BANCO------------");
+        do {            
+            try {
+                
+            System.out.println("1.Registrar cliente."
+                    + "\n2.Mostrar todos los clientes registrados."
+                    + "\n3.Crear cuenta de ahorro."
+                    + "\n4. Crear cuenta corriente."
+                    + "'n4. Consultar cuenta."
+                    + "\n5. Depositar."
+                    + "\n6. Retirar."
+                    + "\n7. Transferir."
+                    + "\n8. Consultar historial"
+                    + "\n9. Salir");
+            
+                System.out.print("Por favor digite una opcion: ");
+                opcion = entrada.nextInt();
+                if(opcion <1 || opcion>9){
+                    //System.out.println("Por favor numeros del 1-9");
+                    JOptionPane.showInputDialog(null, "Por favor numeros del 1-9");
+                }
+                switch (opcion) {
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        
+                        break;
+                        
+                    case 9:
+                        bandera=true;
+                        break;
+                }
+                    
+
+
+
+
+
+
+
+            
+            } catch (Exception e) {
+                System.out.println("Opcion invalida. Digite nuevamente");
+                entrada.nextLine();
+            }
+        
+        } while (bandera==false);
     }
     
 }
